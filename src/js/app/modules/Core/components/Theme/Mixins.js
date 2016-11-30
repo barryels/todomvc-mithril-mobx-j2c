85bb6a0d03a7ebc5a0dc02f2dd58933904935b66
Barry Els <barryels@gmail.com>
2016-11-30 09:42:03 +0200

    adding stub mixin fns for media queries

--- src/js/app/modules/Core/components/Theme/Mixins.js
+++ src/js/app/modules/Core/components/Theme/Mixins.js
@@ -13,6 +13,32 @@ function borderRadius(radius) {
 }
 
 
+function mediaQuery_forPhoneOnly() {
+	// @media (max-width: 599px) { @content; }
+}
+
+function mediaQuery_forTabletPortraitUp() {
+	// @media (min-width: 600px) { @content; }
+}
+
+function mediaQuery_forTabletLandscapeUp() {
+	// @media (min-width: 900px) { @content; }
+}
+
+function mediaQuery_forDesktopUp() {
+	// @media (min-width: 1200px) { @content; }
+}
+
+function mediaQuery_forBigDesktopUp() {
+	// @media (min-width: 1800px) { @content; }
+}
+
+
 module.exports = {
-	borderRadius: borderRadius
+	borderRadius: borderRadius,
+	mediaQuery_forPhoneOnly: mediaQuery_forPhoneOnly,
+	mediaQuery_forTabletPortraitUp: mediaQuery_forTabletPortraitUp,
+	mediaQuery_forTabletLandscapeUp: mediaQuery_forTabletLandscapeUp,
+	mediaQuery_forDesktopUp: mediaQuery_forDesktopUp,
+	mediaQuery_forBigDesktopUp: mediaQuery_forBigDesktopUp
 };
